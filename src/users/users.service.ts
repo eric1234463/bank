b3e9ae702cf5140b507ed59b4949d6f22cb1063c
erickwong <eric.kwong@shoplineapp.com>
2020-10-26 18:12:56 +0800

    feat(nest): init pj

---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[];
+
+  constructor() {
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
